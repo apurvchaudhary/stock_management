5c6c5d04b14ae8b27c9dec0daed46f77724bf01f
Apurv Chaudhary <i1239@Apurvs-MacBook-Pro.local>
2023-01-06 19:12:26 +0530

    added tk formatter class

---
+++ constants.py
@@ -0,0 +1,12 @@
+"""
+GUI constants module
+"""
+
+GUI_TOP_TITLE = "STOCK MANAGEMENT ©2023"
+GUI_IN_TITLE = "Inventory Management by www.apurvchaudhary.com"
+GUI_IN_TITLE_BG_COLOR = "#45A29E"
+GUI_IN_TITLE_FONT = ("Times", "22", "bold")
+GUI_IN_TITLE_HEIGHT = 1
+GUI_IN_TITLE_WIDTH = 100
+GUI_GEOMETRY = "1500x900"
+GUI_BG_COLOR = "#1F2833"
